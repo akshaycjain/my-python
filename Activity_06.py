c33469791b391a2d76fb7b92e64a6429fe7ddf96
akshaycjain <89660378+akshaycjain@users.noreply.github.com>
2021-08-29 01:27:07 +0530

    Update Activity_06.py

--- Activity_06.py
+++ Activity_06.py
@@ -4,7 +4,11 @@ s1=ns[0:3]
 ns[0]=0
 ns[4]=0
 print("sliced list = ",s1)
+s1.reverse()
+print("reversed sliced list = ",s1)
 s1[0]=0
 s1[2]=0
 print("replaced list-1 = ",ns)
+ns.reverse()
+print("reversed list-1 = ",ns)
 print("replaced list-1 = ",s1)
